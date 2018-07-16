24a032ed888676a2202c7b4c8fbed6375a8f83c7
cde <cdechauri@gmail.com>
2018-07-15 21:34:06 -0700

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,140 +1,123 @@
+function searchEventByCategory(){
+    var category = $("select#category").attr('value');
+    console.log(category);
+}
 
 $(document).ready(function () {
 
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-    var APIKey = '&units=imperial&APPID=8ad513190a2f28456f8d69724674498c';
+    //Values from home page dropdown
+    // categoryValue = "";
+    // $('select').on('change',searchEventByCategory).change( searchEventByCategory());
+    $(document).on("click", "select#category", function(){
+        console.log($(this).val())
+    })
 
-
-//Values from home page dropdown
-    categoryValue = "";
-    $('.catVal').on('click', function (event) {
-        console.log(event);
-        categoryValue = $(this).text();
-        console.log("Category = " + categoryValue);
-    });
 
 
     var zipcodeValue = "";
     $('#query').on('change', function (event) {
-        zipcodeValue = $(this).val();
-        console.log(event);
+        zipcodeValue = $(this).val().trim();
         console.log("Zipcode = " + zipcodeValue);
+        var query = "&location=" + zipcodeValue
+        eventfulApi.search(query);
+    });
+
+    $('#search').on('click', function (event) {
+        var zipcodeValue = $('#location').val().trim();
+        var category = $('#category').attr('id')
+
+        console.log("Zipcode = " + zipcodeValue);
+        var query = "&location=" + zipcodeValue + "&c=" + category
+        eventfulApi.search(query);
     });
 
 
-//Eventful API logic
-    var APIKey = "pWNMRCZCjh9HB4nj";
-    var keyword = categoryValue;
-    var pageNumber = 1;
-    var pageSize = 6;
-    var zipcode = zipcodeValue;
-    var date = "Future"
-    var queryURL = "https://api.eventful.com/json/events/search?app_key=" + APIKey +
-        "&keywords=" + keyword +
-        "&page_number=" + pageNumber +
-        "&date=" + date +
-        "&page_size=" + pageSize +
-        "&location=" + zipcode;
-
-    $.ajax({
-        url: "https://safe-headland-27088.herokuapp.com/" + queryURL,
-        method: "GET",
-        "async": true,
-        "crossDomain": true
-    }).then(function (response) {
-
-        var results = JSON.parse(response);
-        console.log(results);
-
-//END Eventful API logic
-
+    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+    var APIKey = '&units=imperial&APPID=8ad513190a2f28456f8d69724674498c';
 
 //Create a function that will generate an HTML string
 //And then add that string to the page
-        function createHTML(zip, tempValue, humidityValue, minTempValue, maxTempValue) {
-            var weatherClass;
+    function createHTML(zip, tempValue, humidityValue, minTempValue, maxTempValue) {
+        var weatherClass;
 
-            var htmlString = '<div class="setBorder ' + weatherClass + '">' +
-                '<div class="weatherCity">' + zip + '</div>' +
-                '<div class="weatherData">' + tempValue + '</div>' +
-                '<div class="weatherHumidity">' + humidityValue + '</div>' +
-                '<div class="weatherMin">' + minTempValue + '</div>' +
-                '<div class="weatherMin">' + maxTempValue + '</div>'
-            '</div>';
-            $('#weatherResults').html(htmlString);
+        var htmlString = '<div class="setBorder ' + weatherClass + '">' +
+            '<div class="weatherCity">' + zip + '</div>' +
+            '<div class="weatherData">' + tempValue + '</div>' +
+            '<div class="weatherHumidity">' + humidityValue + '</div>' +
+            '<div class="weatherMin">' + minTempValue + '</div>' +
+            '<div class="weatherMin">' + maxTempValue + '</div>'
+        '</div>';
+        $('#weatherResults').html(htmlString);
 
-        }
+    }
 
 //Create a function that will execute the Weather AJAX call
-        var searchWeather = function (zip) {
-
-            var searchURL = queryURL + zip + APIKey;
-            $.ajax({
-                url: searchURL,
-                type: 'GET',
-                dataType: 'json',
-                error: function (data) {
-                    console.log("error");
-                    console.log(data.status);
-                    /*alert("Please enter a valid zip code");*/
-                    $('#myModal').modal('show');
-
-                },
-                success: function (data) {
-                    console.log("success");
-                    //Check the browser console to see the returned data
-                    console.log(data);
-                    //Check to make sure the success response is ok
-                    if (data.cod === '404') {
-                        alert("Error. Please enter a valid zip code");
-                        //adding a return will end the success function
-                        return;
-                    }
-
-                    $("#query").val('');
-
-                    var theZip = data.name || '????';
-                    var theTemp = Math.round(data.main.temp) || '????';
-                    var theHumidity = data.main.humidity || '????';
-                    var minTemp = Math.round(data.main.temp_min) || '???';
-                    var maxTemp = Math.round(data.main.temp_max) || '???';
-
-
-                    //Call a function that will create an HTML string & add it to the page
-                    createHTML(
-                        "City: " + theZip,
-                        "Current Temperature: " + theTemp + "(F)",
-                        "Humidity: " + theHumidity + "%",
-                        "Min Temperature: " + minTemp + "(F)",
-                        "Max Temperature: " + maxTemp + "(F)",
-                    );
+    var searchWeather = function (zip) {
+        console.log(zip)
+        var searchURL = queryURL + zip + APIKey;
+        $.ajax({
+            url: searchURL,
+            type: 'GET',
+            dataType: 'json',
+            error: function (data) {
+                console.log("error");
+                console.log(data.status);
+                alert("Please enter a valid zip code");
+            },
+            success: function (data) {
+                console.log("success");
+                //Check the browser console to see the returned data
+                console.log(data);
+                //Check to make sure the success response is ok
+                if (data.cod === '404') {
+                    alert("Error. Please enter a valid zip code");
+                    //adding a return will end the success function
+                    return;
                 }
-            });
-        };
+
+                $("#query").val('');
+
+                var theZip = data.name || '????';
+                var theTemp = Math.round(data.main.temp) || '????';
+                var theHumidity = data.main.humidity || '????';
+                var minTemp = Math.round(data.main.temp_min) || '???';
+                var maxTemp = Math.round(data.main.temp_max) || '???';
+
+
+                //Call a function that will create an HTML string & add it to the page
+                createHTML(
+                    "City: " + theZip,
+                    "Current Temperature: " + theTemp + "(F)",
+                    "Humidity: " + theHumidity + "%",
+                    "Min Temperature: " + minTemp + "(F)",
+                    "Max Temperature: " + maxTemp + "(F)",
+                );
+            }
+        });
+    };
 
 //Code to be executed once the page has fully loaded
 
 
-	//Use jQuery to assign a (callback) function to occur when the 'search' button is clicked
-	$("#search").on('click', function(){
-		console.log("Clicked search");
-		//Use jQuery to get the value of the 'query' input box
-		var newSearchTerm = $("#query").val();
-		console.log(newSearchTerm);
-		//Execute the Weather API call with the 'newSearchTerm' string as its argument 
-		searchWeather(newSearchTerm);
-		$("#search").blur();
-	});
+    //Use jQuery to assign a (callback) function to occur when the 'search' button is clicked
+    $("#search").on('click', function () {
+        console.log("Clicked search");
+        //Use jQuery to get the value of the 'query' input box
+        var newSearchTerm = $("#location").val();
+        console.log(newSearchTerm);
+        //Execute the Weather API call with the 'newSearchTerm' string as its argument
+        searchWeather(newSearchTerm);
+        $("#search").blur();
+    });
 
-	//What if someone just wants to click "ENTER"???
-	//Use jQuery to assign a (callback) function to occur when enter is pressed 
-	//This will ONLY work when the '#query' input box is active
-	$('#query').on('keypress', function(e){
-		//If enter key is pressed
-		if (e.which == 13){
-			//Use jQuery's trigger() function to execute a click event on the '#search' element
-			$("#search").trigger('click');
-		}
-	});
-});
+    //What if someone just wants to click "ENTER"???
+    //Use jQuery to assign a (callback) function to occur when enter is pressed
+    //This will ONLY work when the '#query' input box is active
+    $('#query').on('keypress', function (e) {
+        //If enter key is pressed
+        if (e.which == 13) {
+            //Use jQuery's trigger() function to execute a click event on the '#search' element
+            $("#search").trigger('click');
+        }
+    });
 });
--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -6,65 +6,73 @@ var eventfulApi = {
     location: "San+Francisco",
     pageNumber: 1,
     pageSize: 6,
-    category: "Comedy",
-    getCategories: function () {
-
+    // category: "Comedy",
+    getCategories: function() {
         var listCategories = "http://api.eventful.com/json/categories/list?app_key=xhL49Vr4wmqm32cg"
-        $.ajax({
-            url: "https://safe-headland-27088.herokuapp.com/" + listCategories,
-            method: "GET",
+        $.ajax(  "https://safe-headland-27088.herokuapp.com/" + listCategories, {
+            type: 'GET',
+            dataType: 'json',
             "async": true,
-            "crossDomain": true
-        }).then(function (response) {
-            var results = JSON.parse(response);
-            var categories = results["category"]
-            // const categories = results["category"].map(category => _findText(category.name) );
-            console.log(categories)
+            "crossDomain": true,
+            success: function( response ) {
+                console.log( response );
 
-            var $dropDown = $("<div>", {
-                class: "dropdown-menu show",
-                "aria-labelledby": "category"
-            })
-            categories.forEach(function (category) {
-                var $category = $('<a>', {
-                    class: "dropdown-item catVal",
-                    href: "#",
-                    text: category.name,
-                    id: category.id
+                var categories = response["category"]
+                // const categories = results["category"].map(category => _findText(category.name) );
+
+                var $selectList = $("<select>", {
+                    class: "custom-select",
+                    id: "category"
                 })
-                $dropDown.append($category);
-            })
-            console.log($dropDown);
-            $dropDown;
-        })
+                var $category = $('<option>', {
+                    text: "Choose a Category",
+                    value: "",
+                    placeholder: "Select a Category"
+                })
+                $selectList.append($category);
+                categories.forEach(function (category) {
+                    var $category = $('<option>', {
+                        text: _findText(category.name),
+                        value: category.id,
+                        placeholder: "Select a Category"
+                    })
+                    $selectList.append($category);
+                })
+                $("#categories").append($selectList)
+            },
+            error: function( req, status, err ) {
+                console.log( 'something went wrong', status, err );
+            }
+        });
     },
-
     search: function(query){
-        //Eventful API logic
         // var APIKey = "pWNMRCZCjh9HB4nj";
-        // var keyword = categoryValue;
-        // var pageNumber = 1;
-        // var pageSize = 6;
-        // var zipcode = zipcodeValue;
-        // var date = "Future"
+        console.log(query)
+        //Eventful API logic
         var queryURL = "https://api.eventful.com/json/events/search?app_key=" + this.apiKey +
-            "&c=" + this.category +
+            // "&c=" + this.category +
             "&page_number=" + this.pageNumber +
             // "&date=" + this.date +
             "&page_size=" + this.pageSize +
-            "&location=" + this.location;
+            "&location=" + this.location; +
 
-        $.ajax({
-            url: "https://safe-headland-27088.herokuapp.com/" + queryURL,
-            method: "GET",
+            query
+
+        $.ajax(  "https://safe-headland-27088.herokuapp.com/" + queryURL, {
+            type: 'GET',
+            dataType: 'json',
             "async": true,
-            "crossDomain": true
-        }).then(function (response) {
-            var results = JSON.parse(response);
-            console.log(results)
-            displayCardDeck(results["events"].event)
-        })
+            "crossDomain": true,
+            success: function( response ) {
+                console.log( response );
+                displayCardDeck(response["events"].event)
+            },
+            error: function( req, status, err ) {
+                console.log( 'something went wrong', status, err );
+            }
+        });
     }
+
 }
 
 function _findText(str) {
@@ -73,75 +81,82 @@ function _findText(str) {
         .replace(/&quot;/g,'"')
 }
 
-function displayCategories(){
-    console.log("here")
-    // <div class="dropdown-menu" aria-labelledby="category">
-    //     <a class="dropdown-item catVal" href="#">Music</a>
-    //     <a class="dropdown-item catVal" href="#">Cooking</a>
-    //     <a class="dropdown-item catVal" href="#">Sports</a>
-    //     <a class="dropdown-item catVal" href="#">Paintball</a>
-    //     <a class="dropdown-item catVal" href="#">Go Kart</a>
-    // </div>
-    var $dropDown = $('<div>', {
-        class: "dropdown-menu",
-        "aria-labelledby": "category"
-    })
-    var $categories = $('#categories')
-    // $('#categories').append(eventfulApi.getCategories());
-
-    // categories.forEach(function (category) {
-    //     var $category = $('<a>', {
-    //         class: "dropdown-item catVal",
-    //         href: "#",
-    //         text: category
-    //     })
-    //     $dropDown.append($category);
-    // })
-
-    var $category1 = $('<a>', {
-        class: "dropdown-item catVal",
-        href: "#",
-        text: "music",
-        id: "music"
-    })
-    var $category2 = $('<a>', {
-        class: "dropdown-item catVal",
-        href: "#",
-        text: "education",
-        id: "music"
-    })
-    console.log($category1)
-    $dropDown.append($category1);
-    $dropDown.append($category2);
-    console.log($dropDown)
-    $categories.append($dropDown)
-}
-
-
 function displayCardDeck(events){
-    console.log(events)
     var $cardDeck = $('.events-grid')
-
+    $cardDeck.empty();
     events.forEach(function(event){
         var cardObj = new Card(event);
-        // console.log(cardObj.createCard())
-
-        $cardDeck.append(cardObj.createCard())
+        cardObj.createCard()
     })
+}
+
+function displayCategories(){
+    var $selectList = $("<select>", {
+        class: "custom-select",
+        id: "category"
+    })
+    var $category = $('<option>', {
+        text: "Choose a Category",
+        value: "",
+        placeholder: "Select a Category"
+    })
+    $selectList.append($category);
+    categories["category"].forEach(function (category) {
+
+        var $category = $('<option>', {
+            text: _findText(category.name),
+            value: category.id,
+            placeholder: "Select a Category"
+        })
+        $selectList.append($category);
+    })
+    $("#categories").append($selectList)
+//     $("#categories").on('change', function(){
+//         console.log(this)
+//         var categoryValue = $(this).value;
+//         console.log("Category = " + categoryValue);
+//     })
 }
 
 $(document).ready(function() {
+    displayCategories()
+    // eventfulApi.getCategories()
+    // eventfulApi.search()
 
-    eventfulApi.search()
-    eventfulApi.getCategories()
-
-    $("#add-search").on("click", function(e) {
+    $("#search").on("click", function(e) {
         e.preventDefault();
-
-        eventfulApi.search();
+        var category = $('#category').attr("id")
+        var zipCode = $('#location').val()
+        eventfulApi.search("&l=" + zipCode);
 
         return false;
     })
+
+    if ("geolocation" in navigator) {
+        /* geolocation is available */
+    //    set zipcode and map
+    } else {
+        /* geolocation IS NOT available */
+    //    set default bay area code
+    }
+
+
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //     console.log(position)
+    //     // do_something(position.coords.latitude, position.coords.longitude);
+    // });
+    //
+    //
+    // var watchID = navigator.geolocation.watchPosition(function(position) {
+    //     console.log(position)
+    //     // console.log(position.coords.latitude + " " + position.coords.longitude);
+    //
+    // });
+
 })
 
 
+
+//
+
+var categories = {"category":[{"name":"Concerts &amp; Tour Dates","event_count":null,"id":"music"},{"name":"Conferences &amp; Tradeshows","event_count":null,"id":"conference"},{"name":"Comedy","event_count":null,"id":"comedy"},{"name":"Education","event_count":null,"id":"learning_education"},{"name":"Kids &amp; Family","event_count":null,"id":"family_fun_kids"},{"name":"Festivals","event_count":null,"id":"festivals_parades"},{"name":"Film","event_count":null,"id":"movies_film"},{"name":"Food &amp; Wine","event_count":null,"id":"food"},{"name":"Fundraising &amp; Charity","event_count":null,"id":"fundraisers"},{"name":"Art Galleries &amp; Exhibits","event_count":null,"id":"art"},{"name":"Health &amp; Wellness","event_count":null,"id":"support"},{"name":"Holiday","event_count":null,"id":"holiday"},{"name":"Literary &amp; Books","event_count":null,"id":"books"},{"name":"Museums &amp; Attractions","event_count":null,"id":"attractions"},{"name":"Neighborhood","event_count":null,"id":"community"},{"name":"Business &amp; Networking","event_count":null,"id":"business"},{"name":"Nightlife &amp; Singles","event_count":null,"id":"singles_social"},{"name":"University &amp; Alumni","event_count":null,"id":"schools_alumni"},{"name":"Organizations &amp; Meetups","event_count":null,"id":"clubs_associations"},{"name":"Outdoors &amp; Recreation","event_count":null,"id":"outdoors_recreation"},{"name":"Performing Arts","event_count":null,"id":"performing_arts"},{"name":"Pets","event_count":null,"id":"animals"},{"name":"Politics &amp; Activism","event_count":null,"id":"politics_activism"},{"name":"Sales &amp; Retail","event_count":null,"id":"sales"},{"name":"Science","event_count":null,"id":"science"},{"name":"Religion &amp; Spirituality","event_count":null,"id":"religion_spirituality"},{"name":"Sports","event_count":null,"id":"sports"},{"name":"Technology","event_count":null,"id":"technology"},{"name":"Other &amp; Miscellaneous","event_count":null,"id":"other"}]}

5aa05c00dc49f6a6aa679d43eb50c84c7ac7f4ff
Shoaib Naeem <you@example.com>
2018-07-16 22:10:07 -0700

    made weather results show on bottom

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
             error: function (data) {
                 console.log("error");
                 console.log(data.status);
-                alert("Please enter a valid zip code");
+                $('#myModal').modal('show');
             },
             success: function (data) {
                 console.log("success");
